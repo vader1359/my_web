9deb3492431410c23895b258227a95f47f232d48
vader1359 <vader1359@gmail.com>
2017-10-06 03:57:30 +0700

    Finish resume page still not responsive nav bar

---
+++ app/controllers/resume_controller.rb
@@ -0,0 +1,6 @@
+class ResumeController < ApplicationController
+  def index
+  end
+  def test
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'resume/index'
+  get 'resume/test'
+
+  root 'resume#test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
