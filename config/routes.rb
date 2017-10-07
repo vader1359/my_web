3e7761923f2d2eeb69cf83639e1af596592e7e98
vader1359 <vader1359@gmail.com>
2017-10-08 06:55:00 +0700

    Nearly done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'resume/index'
   get 'resume/test'
 
+  resources :portfolio
+
   root 'resume#test'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
