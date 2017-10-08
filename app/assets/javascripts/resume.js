0e0cb306a2a12443c1fc29af13d317e979ef9799
vader1359 <vader1359@gmail.com>
2017-10-08 20:33:40 +0700

    hidden css

--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -1,6 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 document.addEventListener("turbolinks:load", function() {
-  $("body").hide();
   $("body").fadeIn(1000);
 });
