5307ea4aa301975b73b69c8e65078e7e013655c4
vader1359 <vader1359@gmail.com>
2017-10-08 20:31:24 +0700

    Add pace

--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -1,8 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 document.addEventListener("turbolinks:load", function() {
-  $("img").hide();
-  $("a").hide();
-  $("img").fadeIn(1000);
-  $("a").fadeIn(1200);
+  $("body").hide();
+  $("body").fadeIn(1000);
 });
