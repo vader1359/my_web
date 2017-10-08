3f1da69d4d2a9e679886fbe62c2a2747e2aa863a
vader1359 <vader1359@gmail.com>
2017-10-08 19:45:06 +0700

    contact left

--- app/assets/javascripts/resume.js
+++ app/assets/javascripts/resume.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener("turbolinks:load", function() {
+  $("img").hide();
+  $("a").hide();
+  $("img").fadeIn(1000);
+  $("a").fadeIn(1200);
+});
